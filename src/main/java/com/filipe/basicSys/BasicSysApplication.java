5cdd074af3b86299ffb95c8c45aa8f140e2ebe52
Filipe <filipe.so137@gmail.com>
2025-05-21 14:48:03 -0300

    estrutura basica

---
+++ src/main/java/com/filipe/basicSys/BasicSysApplication.java
@@ -0,0 +1,13 @@
+package com.filipe.basicSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSysApplication.class, args);
+	}
+
+}
